cb6d6af3a1da939404958cb49828ff0df111fd23
Lucas <lucasfelicio378@gmail.com>
2022-07-18 16:29:37 -0300

    finalizado

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Topo from "./components/Topo"
+import Top_935 from "./components/Topo935"
+import Top_desktop from "./components/TopoDesktop"
+import Feed from "./components/Feed"
+
+export default function Container(){
+    console.log("foi")
+    return(
+        <div class='container'>
+            <Topo />
+            <Top_935 />
+            <Top_desktop />
+            <div className="compensando-topo"></div>
+            <Feed />
+            
+
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import ReactDOM from 'react-dom';
-import Topo from "./components/Topo"
-import Top_935 from "./components/Topo935"
-import Top_desktop from "./components/TopoDesktop"
-import Feed from "./components/Feed"
+import ReactDOM from "react-dom";
 
-function Container(){
-    console.log("foi")
-    return(
-        <div class='container'>
-            <Topo />
-            <Top_935 />
-            <Top_desktop />
-            <div className="compensando-topo"></div>
-            <Feed />
-            
+import App from "./App";
 
-        </div>
-    );
-}
-ReactDOM.render(<Container />, document.querySelector(".root"));
+ReactDOM.render(<App />, document.querySelector(".root"));
